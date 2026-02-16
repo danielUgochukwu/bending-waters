887f9d86ef6c39d3d7dde23799d45544b116a4d5
Daniel Ugochukwu <slade8546@gmail.com>
2026-02-16 13:53:46 +0100

    feat: Add Open Graph image generation for social sharing.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,33 +1,39 @@
 import { ImageResponse } from 'next/og';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 // Image metadata
 export const alt = 'BendingWaters';
 export const size = {
-  width: 1200,
-  height: 630,
+    width: 1200,
+    height: 630,
 };
 export const contentType = 'image/png';
 
 // Image generation
 export default async function Image() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <img src="/images/logo.png" alt="BendingWaters" />
-      </div>
-    ),
-    {
-      ...size,
-    }
-  );
+    const logoPath = join(process.cwd(), 'public/images/logo.png');
+    const logoData = readFileSync(logoPath);
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 128,
+                    background: 'white',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                {/* @ts-ignore */}
+                <img src={logoData.buffer} alt="BendingWaters" />
+            </div>
+        ),
+        {
+            ...size,
+        }
+    );
 }
